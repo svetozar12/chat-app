0f19dc2477640631f24ffebc1009482cd85b934d
svetozar12 <zaauna@abv.bg>
2021-09-10 15:32:09 +0300

    made something loke auth

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import axios from "axios";
+import { useCookie } from "next-cookie";
+import { GetServerSideProps } from "next";
 import { AppProps } from "next/dist/shared/lib/router/router";
+import { useRouter } from "next/router";
+import ChatRoom from "../components/ChatRoom";
 
-function login() {
+function login(props: AppProps) {
+  const router = useRouter();
+  const cookie = useCookie(props.cookie);
   const [name, setName] = React.useState("");
   const [alert, setAlert] = React.useState("");
 
@@ -10,19 +16,27 @@ function login() {
     try {
       const res = await axios.get(`http://localhost:4001/users/${name}`);
       console.log(res);
+      return true;
     } catch (error: any) {
+      console.log(error);
       setAlert(error.response.data.message.message);
+      return false;
     }
   };
 
-  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     if (name) {
-      loginPost();
-      setName("");
+      const result = await loginPost();
+      if (result) {
+        // console.log(name);
+        cookie.set("name", name);
+        router.push("/chatRoom");
+      }
       setTimeout(() => {
         setAlert("");
       }, 2000);
+      console.log(name);
     } else {
       setAlert("No input");
       setTimeout(() => {
@@ -32,21 +46,33 @@ function login() {
   };
 
   return (
-    <form style={{ height: "100vh" }} className="container">
-      <h1 style={{ color: "red" }}>{alert}</h1>
-      <h1>Login</h1>
-      <input
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        type="text"
-        name="username"
-        placeholder="username ..."
-      />
-      <button onClick={handleSubmit} type="submit">
-        login
-      </button>
-    </form>
+    <>
+      <h1>
+        <form style={{ height: "100vh" }} className="container">
+          <h1 style={{ color: "red" }}>{alert}</h1>
+          <h1>Login</h1>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            name="username"
+            placeholder="username ..."
+          />
+          <button onClick={handleSubmit} type="submit">
+            login
+          </button>
+        </form>
+      </h1>
+    </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookie = useCookie(context);
+
+  return {
+    props: { cookie: context.req.headers.cookie || "" },
+  };
+};
 
 export default login;
