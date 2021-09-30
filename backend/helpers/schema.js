5f5cee9fb353ae62733ef007982258c952f62682
George Pasquali <gpasquali@hilscher.com>
2021-09-30 11:54:24 +0300

    removed js files from the src backend folder

--- backend/helpers/schema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Joi = require("joi");
-var registerValidation = function (data) {
-    var schema = Joi.object({
-        username: Joi.string().min(2).max(20).required().label("Invalid input"),
-    });
-    return schema.validate(data);
-};
-module.exports.registerValidation = registerValidation;
