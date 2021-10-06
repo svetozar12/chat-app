89b90e2f3c1ffc99e520a95976faa7a007156202
svetozar12 <zaauna@abv.bg>
2021-10-06 09:47:49 +0300

    most of the endpoints are finished now will start implementing them in the frontend

--- backend/src/models/chatRoom.model.ts
+++ backend/src/models/chatRoom.model.ts
@@ -1,14 +1,20 @@
 import * as mongoose from "mongoose";
 
 const chatRoomSchema = new mongoose.Schema({
-  members: {
-    type: String,
-    required: true,
-  },
-  messages: {
-    type: String,
-    required: true,
-  },
+  members: [
+    {
+      user1: { type: String, min: 2, max: 10, required: true },
+      user2: { type: String, min: 2, max: 10, required: true },
+    },
+  ],
+  messages: [
+    {
+      sender: { type: String, required: true },
+      time_stamp: { type: String, min: 2, max: 10, required: true },
+      message: { type: String, min: 1, max: 100, required: true },
+      seenBy: [String],
+    },
+  ],
 });
 
 const chatRoom = mongoose.model("chatRoom", chatRoomSchema);
--- backend/src/router/chatRoom_route/chatRoom_route.tsx
+++ backend/src/router/chatRoom_route/chatRoom_route.tsx
@@ -3,15 +3,100 @@ const route = express.Router();
 const createError = require("http-errors");
 
 import { Request, Response } from "express";
+import { read } from "fs";
 
-const User = require("../../models/User.model");
+const Chats = require("../../models/chatRoom.model");
 
-route.get("/", (req: Request, res: Response) => {
+route.get("/chat-room/users-rooms", async (req: Request, res: Response) => {
   try {
-    const chat = new User({ members: "user1", messages: "hi" });
-    return res.send("good!");
+    const users_rooms = await Chats.find({}).exec();
+    if (!users_rooms || users_rooms.length <= 0)
+      return res.status(404).json({ Message: "User rooms not found" });
+    return res.status(200).json({ Message: users_rooms });
   } catch (error) {
-    return res.send("bad!");
+    console.log(error);
+    return res
+      .status(501)
+      .json({ Message: "Something went wrong while getting the users" });
+  }
+});
+
+route.post("/chat-room/messages", async (req: Request, res: Response) => {
+  try {
+    const today = new Date();
+    const date = today.getHours() + ":" + today.getMinutes();
+    const result = today.toLocaleDateString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const user1 = req.body.user1;
+    const user2 = req.body.user2;
+    const sender = req.body.sender;
+    const message = req.body.message;
+
+    const chat = await new Chats({
+      members: [{ user1, user2 }],
+      messages: [
+        {
+          sender,
+          time_stamp: result,
+          message,
+          seenBy: [],
+        },
+      ],
+    });
+
+    await chat.save();
+
+    return res.json({ Message: chat });
+  } catch (error) {
+    console.log(error);
+
+    return res
+      .status(501)
+      .json({ Message: "Something went wrong while sending the message" });
+  }
+});
+
+route.post("/chat-room/seen_by", async (req: Request, res: Response) => {
+  try {
+    const readBy = req.query.readBy;
+    const chat = await Chats.findOne({ _id: req.body.id });
+    if (!chat) {
+      return res.status(404).json({ Message: "error 404 not found" });
+    }
+    if (readBy == null) return;
+    chat.messages.forEach((element) => {
+      const found = element.seenBy.find((element) => element === readBy);
+      console.log(found);
+      if (found)
+        return res
+          .status(409)
+          .json({ Message: `User ${readBy} already saw the message` });
+
+      element.seenBy.push(readBy);
+    });
+
+    await chat.save();
+
+    return res
+      .status(201)
+      .json({ Message: `User ${readBy} saw the message`, chat });
+  } catch (error) {
+    return res
+      .status(501)
+      .json({ Message: "Something went wrong while seeing the message" });
+  }
+});
+
+route.delete("/chat-room/dev-delete", async (req: Request, res: Response) => {
+  try {
+    const deleteChats = await Chats.deleteMany({});
+    return res.status(204);
+  } catch (error) {
+    res
+      .status(501)
+      .json({ Message: "Something went wrong while deleting the chats" });
   }
 });
 
