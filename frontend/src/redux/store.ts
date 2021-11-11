7fb02abe2deb080009b6f1fe58a41a1cc14efcf8
svetozar12 <zaauna@abv.bg>
2021-11-11 18:51:11 +0200

    implemented redux-persist libary

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,11 +1,23 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import reducers from "./reducer";
-export const store = createStore(
-  reducers,
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = createStore(
+  persistedReducer,
   {},
   composeWithDevTools(applyMiddleware(thunk)),
 );
+const persistor = persistStore(store);
 
 export * as actions from "./actions/authActions";
+export { persistor, store };
