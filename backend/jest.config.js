1e95efb2754e7530bcfa9e3a98407e00cdebc6c1
Svetozar Gospodinov <zaauna@abv.bg>
2021-11-15 21:58:19 +0200

    removing comments

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,16 +1,3 @@
-// module.exports = {
-//   globals: {
-//     "ts-jest": {
-//       tsconfig: "tsconfig.json",
-//     },
-//   },
-//   moduleFileExtensions: ["ts", "js"],
-//   transform: {
-//     "^.+\\.(ts|tsx)$": "ts-jest",
-//   },
-//   testMatch: ["**/test/**/*.test.(ts|js)"],
-//   testEnvironment: "node",
-// };
 module.exports = {
   globals: {
     "ts-jest": {
