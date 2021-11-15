c290321a4695bd32abdbc62c442c55fb48be360c
Svetozar Gospodinov <zaauna@abv.bg>
2021-11-15 21:55:30 +0200

    created first test in the backend

--- backend/__test__/users.test.ts
+++ backend/__test__/users.test.ts
@@ -1,8 +1,18 @@
 import { app } from "../src/server";
 import * as request from "supertest";
 
-describe("GET /users/test1", () => {
-  it("should return 200 OK", () => {
-    return request(app).get("/users/test1").expect(200);
+describe("Valid input on login", () => {
+  it("should return 200 OK", async () => {
+    const res = await request(app).get("/users/test1");
+    expect(res.status).toBe(200);
+    expect(res.body.message.username).toBe("test1");
+  });
+});
+
+describe("Invalid input on login", () => {
+  it("should return 200 OK", async () => {
+    const res = await request(app).get("/users/11111");
+    expect(res.status).toBe(404);
+    expect(res.body.ERROR).toBe("Invalid input");
   });
 });
