bad76ba3328d0233b9c53f519507c56e4c24cb89
Svetozar Gospodinov <zaauna@abv.bg>
2021-11-23 08:54:02 +0200

    quick fix on one of the tests,modified gitignore

--- packages/backend/__test__/GET/users_route.test.ts
+++ packages/backend/__test__/GET/users_route.test.ts
@@ -1,12 +1,32 @@
 import { app } from "../../src/server";
 import * as request from "supertest";
+import User from "../../src/models/User.model";
 import "jest";
+
+beforeAll(async () => {
+  try {
+    const users = new User({ username: "TestUser" });
+    users.save();
+    return true;
+  } catch (error) {
+    return false;
+  }
+});
+
+afterAll(async () => {
+  try {
+    await User.deleteOne({ username: "TestUser" });
+    return true;
+  } catch (error) {
+    return false;
+  }
+});
 
 describe("Valid input on login", () => {
   it("should return 200 OK", async () => {
-    const res = await request(app).get("/users/test1");
+    const res = await request(app).get("/users/TestUser");
     expect(res.status).toBe(200);
-    expect(res.body.message.username).toBe("test1");
+    expect(res.body.message.username).toBe("TestUser");
   });
 });
 
--- packages/backend/src/app.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./server";
-
-const port: number = 4001;
-
-app.listen(port, (): void => {
-  console.log(`listening on port ${port}`);
-});
