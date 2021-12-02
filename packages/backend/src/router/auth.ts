ab98177fb5b7a5677ee9ae1ecbb309531c7227ad
svetozar12 <zaauna@abv.bg>
2021-12-03 01:47:06 +0200

    added JWT for api endpoint

---
+++ packages/backend/src/router/auth.ts
@@ -0,0 +1,72 @@
+import * as express from "express";
+import { Request, Response } from "express";
+import type { RequestHandler } from "express";
+import * as jwt from "jsonwebtoken";
+import registerValidation from "../helpers/schema";
+import User from "../models/User.model";
+
+const route = express.Router();
+
+const verifyToken: RequestHandler = (req: any, res, next) => {
+  const bearerHeader = req.headers["authorization"];
+  if (typeof bearerHeader !== "undefined") {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+};
+
+route.get("/user", verifyToken, async (req: any, res: Response) => {
+  try {
+    jwt.verify(req.token, "secretKey", (err: any, authData: any) => {
+      if (err) {
+        console.log(err);
+
+        return res.sendStatus(403);
+      } else {
+        return res.json({
+          authData,
+        });
+      }
+    });
+
+    return res.status(200);
+  } catch (error) {
+    return res.status(501).json({
+      ErrorMsg: (error as Error).message,
+      Error: "Internal server error",
+      Message: "Something went wrong while loging",
+    });
+  }
+});
+
+route.post("/login", async (req: Request, res: Response) => {
+  try {
+    const username = req.body.username;
+    console.log(req.body);
+
+    const user = {
+      username,
+    };
+    jwt.sign({ user }, "secretKey", { expiresIn: "1hr" }, (err, token) => {
+      if (err) res.status(403); //Unauthorized 403
+      return res
+        .json({
+          token,
+        })
+        .status(201);
+    });
+  } catch (error) {
+    return res.status(403).json({
+      //Unauthorized 403
+      ErrorMsg: (error as Error).message,
+      Error: "Internal server error",
+      Message: "Something went wrong while deleting",
+    });
+  }
+});
+
+export { route as auth };
--- packages/backend/src/router/router.ts
+++ packages/backend/src/router/router.ts
@@ -5,10 +5,12 @@ import { route as users_route } from "./users_route/users_route";
 import { route as invite_route } from "./invite_route/invite_route";
 import { route as chat_route } from "./chatRoom_route/chatRoom_route";
 import { route as messages } from "./messages_route/messages";
+import { auth } from "./auth";
 
 route.use("/", users_route);
 route.use("/", invite_route);
 route.use("/", chat_route);
 route.use("/", messages);
+route.use("/auth", auth);
 
 export default route;
