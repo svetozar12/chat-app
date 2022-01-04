b2ae792a0668485845a4fb494f016d2cbb421e2e
svetozar12 <zaauna@abv.bg>
2022-01-05 00:36:34 +0200

    unit tests for index page hrefs

--- packages/frontend/__tests__/pages_tests/index.test.tsx
+++ packages/frontend/__tests__/pages_tests/index.test.tsx
@@ -1,11 +1,38 @@
-import { screen, render } from "@testing-library/react";
+import { screen, render, RenderResult } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "../../src/pages/index";
+import Router from "next/router";
 
 describe("index page", () => {
+  const spies: any = {};
+  let container: RenderResult;
+  beforeEach(() => {
+    container = render(<Home />);
+    spies.routerChangeStart = jest.fn();
+    Router.events.on("routeChangeStart", spies.routerChangeStart);
+  });
+
+  afterEach(() => {
+    Router.events.off("routeChangeStart", spies.routerChangeStart);
+  });
   it("renders index page", async () => {
-    render(<Home />);
     const headingElement = screen.getByText("Create an account !");
     expect(headingElement).toBeInTheDocument();
+  });
+  test("click on link create account and redirect", async () => {
+    const register_href = container
+      .getByText("Create an account !")
+      .closest("a");
+    expect(register_href).toHaveAttribute(
+      "href",
+      "http://localhost:3000/register",
+    );
+  });
+
+  test("click on link login into account and redirect", async () => {
+    const login_href = container
+      .getByText("Already have a acount ?")
+      .closest("a");
+    expect(login_href).toHaveAttribute("href", "http://localhost:3000/login");
   });
 });
