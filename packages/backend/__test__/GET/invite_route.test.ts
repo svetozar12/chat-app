9b825a0f986bfe723790b9fb52a070e84eff3610
svetozar12 <zaauna@abv.bg>
2022-06-01 14:24:10 -0700

    tests

--- packages/backend/__test__/GET/invite_route.test.ts
+++ packages/backend/__test__/GET/invite_route.test.ts
@@ -39,6 +39,44 @@ afterAll(async () => {
   }
 });
 
+describe("valid invites for user :/invites/:id?status=accepted", () => {
+  it("should return 200 ok", async () => {
+    const res = await request(app)
+      .get(`/invites/${user1.user_id}?status=accepted`)
+      .set({ Authorization: `Bearer ${user1.Access_token}` });
+
+    res.body.invites.forEach((element: { status: string }) => {
+      expect(element.status).toBe("accepted");
+    });
+    // expect(res.status).toBe(401);
+    // expect(res.body.ErrorMsg).toBe("Can't access other users data");
+  });
+});
+
+describe("valid invites for user :/invites/:id?status=accepted", () => {
+  it("should return 200 ok", async () => {
+    const res = await request(app)
+      .get(`/invites/${user1.user_id}?status=recieved`)
+      .set({ Authorization: `Bearer ${user1.Access_token}` });
+
+    res.body.invites.forEach((element: { status: string }) => {
+      expect(element.status).toBe("recieved");
+    });
+  });
+});
+
+describe("valid invites for user :/invites/:id?status=accepted", () => {
+  it("should return 200 ok", async () => {
+    const res = await request(app)
+      .get(`/invites/${user1.user_id}?status=declined`)
+      .set({ Authorization: `Bearer ${user1.Access_token}` });
+
+    res.body.invites.forEach((element: { status: string }) => {
+      expect(element.status).toBe("declined");
+    });
+  });
+});
+
 describe("Bad input/Non existing invites for user :/invites/:id/", () => {
   it("should return 404 Not Found", async () => {
     const res = await request(app)
