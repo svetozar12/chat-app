0e9615c5f678d84200b0772896d288cc0f4c9e25
Svetozar Gospodinov <sgospodinov@hilscher.com>
2022-06-14 17:07:02 +0300

    ,

--- packages/frontend/src/components/ChatRoom/ChatRoom.tsx
+++ packages/frontend/src/components/ChatRoom/ChatRoom.tsx
@@ -18,21 +18,10 @@ import { useCookie } from "next-cookie";
 import api_helper from "../../services/graphql/api_helper";
 import { InitialStateMessage } from "../../services/redux/reducer/messageReducer/state";
 import { IInitialSet } from "../../services/redux/reducer/setReducer/state";
-
-const Message_form = styled.form`
-  width: 100%;
-  background: var(--main-white);
-  height: 10%;
-`;
+import ChatRoomForm from "./ChatRoomForm/ChatRoomForm";
 
 interface IHome {
   chatId: any;
-}
-
-interface IPropsState {
-  name?: string;
-  message?: string;
-  time?: string | number;
 }
 
 export interface IchatInstance {
@@ -48,17 +37,11 @@ const ChatRoom: NextPage<IHome> = ({ chatId }) => {
   const statess = useSelector((state: { setReducer: IInitialSet }) => state.setReducer);
   const inputTextArea = React.useRef<any>(null);
   const cookie = useCookie();
-  const cookieName = cookie.get("name") as string;
   const user_id = cookie.get("id") as string;
   const token = cookie.get("token") as string;
   const dispatch = useDispatch();
   const [socketRef, setSocketRef] = useState<Socket | null>(null);
   const containerRef = React.useRef<null | HTMLDivElement>(null);
-  const [state, setState] = useState<IPropsState>({
-    name: cookie.get("name"),
-    message: "",
-    time: "",
-  });
 
   const getRecentMessages = async () => {
     try {
@@ -100,15 +83,6 @@ const ChatRoom: NextPage<IHome> = ({ chatId }) => {
     getRecentMessages();
   }, [route.asPath]);
 
-  const saveMessage = async () => {
-    try {
-      await api_helper.message.create(user_id, chatId, state.message as string, token);
-      return true;
-    } catch (error) {
-      return false;
-    }
-  };
-
   const scrollHandler = async (e: React.UIEvent<HTMLElement>) => {
     try {
       if (e.currentTarget.scrollTop === 0) {
@@ -143,35 +117,6 @@ const ChatRoom: NextPage<IHome> = ({ chatId }) => {
     scrollToBottom();
   }, [messageState.messages]);
 
-  const onMessageSubmit = async (e: React.FormEvent<HTMLFormElement> | React.MouseEvent<SVGElement>) => {
-    e.preventDefault();
-    if (state.message) {
-      const { name, message, time } = state;
-
-      await saveMessage();
-      socketRef?.emit("message", {
-        chatInstance: chatId,
-        sender: cookieName,
-        message,
-        time,
-      });
-      setState({ name, message: "" });
-    }
-  };
-
-  const handleKeyPress = (e: any) => {
-    const target = e.target as HTMLTextAreaElement;
-    inputTextArea.current.style.height = "15px";
-    inputTextArea.current.style.height = `${target.scrollHeight}px`;
-    inputTextArea.current.style.height = `${Math.min(e.target.scrollHeight, 60)}px`;
-
-    setState({ ...state, [e.target.name]: e.target.value });
-  };
-  const handleSubmit = (e: any) => {
-    if (e.key === "Enter") {
-      onMessageSubmit(e);
-    }
-  };
   return (
     <div
       className={css`
@@ -210,76 +155,7 @@ const ChatRoom: NextPage<IHome> = ({ chatId }) => {
           );
         })}
       </div>
-
-      <Message_form onSubmit={onMessageSubmit} className="flex">
-        <div
-          className={css`
-            cursor: text;
-            position: relative;
-            z-index: 1000;
-            flex-direction: row;
-            justify-content: space-between;
-            background: #d9d9d9;
-            border-radius: 25px;
-            width: 70%;
-            margin: 0.5rem 0;
-            padding: 0.5rem 2rem;
-            overflow-wrap: break-word;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          flex`}
-          onClick={() => inputTextArea.current.focus()}
-        >
-          <textarea
-            className={css`
-              border: none;
-              resize: none;
-              padding: 0;
-              wordbreak: break-all;
-              width: 90%;
-              background: transparent;
-              height: 0.9375rem;
-              &:focus {
-                outline: none;
-              }
-            `}
-            ref={inputTextArea}
-            name="message"
-            onKeyDown={(e) => handleSubmit(e)}
-            onChange={(e) => handleKeyPress(e)}
-            placeholder="Your Message "
-            value={state.message}
-          />
-          <div
-            className={css`
-              display:flex;
-              justify-content: center;
-              align-items: center;
-              cursor: pointer;
-              padding: 0.3rem 0 0.3rem 0.5rem;
-              border: 1px transperant;
-              border-radius: 50px;
-              &:hover {
-                background: rgba(0, 0, 0, 0.1);
-                border-radius: 50px;
-              }
-            flex`}
-          >
-            <MdSend
-              className={css`
-                cursor: pointer;
-                width: 1.5rem;
-                height: 1.5rem;
-                margin-right: 0.3rem;
-                padding: 0.1rem;
-              `}
-              type="submit"
-              onClick={onMessageSubmit}
-            />
-          </div>
-        </div>
-      </Message_form>
+      <ChatRoomForm chatId={chatId} socketRef={socketRef} inputTextArea={inputTextArea} />
     </div>
   );
 };
