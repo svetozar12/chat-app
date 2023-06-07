84ef3c41c2c4d9d4a9c6a9d2c3ffe7c2436537c8
svetozar12 <zaauna@abv.bg>
2023-06-07 15:00:40 -0700

    .

--- libs/web/env/src/lib/web-env.ts
+++ libs/web/env/src/lib/web-env.ts
@@ -1,21 +1,23 @@
 export interface CommonEnvs {
   NEXT_PUBLIC_OAUTH_METHODS: string[];
-  API_URL: string;
   NEXT_PUBLIC_WS_SERVER_URL: string;
 }
 
-export type ClientEnvs = object;
+export type ClientEnvs = {
+  NEXT_API_URL: string;
+};
 
 export type ServerEnvs = object;
 
 export const commonEnvs: CommonEnvs = {
   NEXT_PUBLIC_OAUTH_METHODS:
     process.env['NEXT_PUBLIC_OAUTH_METHODS']?.split(',') || [],
-  API_URL: process.env['API_URL'] || '',
   NEXT_PUBLIC_WS_SERVER_URL:
     process.env['NEXT_PUBLIC_WS_SERVER_URL'] || 'ws://localhost:3000',
 };
 
-export const clientEnvs: ClientEnvs = {};
+export const clientEnvs: ClientEnvs = {
+  NEXT_API_URL: process.env['NEXT_API_URL'] || '',
+};
 
 export const serverEnvs: ServerEnvs = {};
