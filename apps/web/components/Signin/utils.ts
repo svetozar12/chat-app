1432ce19e52a6b304879fd353b7baa50a308959a
svetozar12 <zaauna@abv.bg>
2023-06-07 15:01:35 -0700

    .

--- apps/web/components/Signin/utils.ts
+++ apps/web/components/Signin/utils.ts
@@ -1,22 +1,22 @@
-import { commonEnvs } from '@chat-app/web/env';
+import { clientEnvs } from '@chat-app/web/env';
 import { AiOutlineGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 
-const { API_URL } = commonEnvs;
+const { NEXT_API_URL } = clientEnvs;
 
 export const renderOauthButtons = [
   {
     Icon: AiOutlineGithub,
     title: 'Sign in with Github',
     onClick: () => {
-      window.open(`${API_URL}/auth/github`, '_self');
+      window.open(`${NEXT_API_URL}/auth/github`, '_self');
     },
   },
   {
     Icon: FcGoogle,
     title: 'Sign in with Google',
     onClick: () => {
-      window.open(`${API_URL}/auth/google`, '_self');
+      window.open(`${NEXT_API_URL}/auth/google`, '_self');
     },
   },
 ];
