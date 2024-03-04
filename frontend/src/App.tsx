dfd29af55d93fe135375702d57c919cd475e730b
svetozar12 <s.gospodinov02@gmail.com>
2024-03-04 15:19:32 +0200

    added nx to the project

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { socket } from "./socket";
-import { TestWs } from "./components/TestWs";
-
-export default function App() {
-  return <TestWs></TestWs>;
-}
--- frontend/src/socket.ts
+++
@@ -1,5 +0,0 @@
-import { io } from "socket.io-client";
-
-const URL = "ws://localhost:8080";
-
-export const socket = io(URL);
